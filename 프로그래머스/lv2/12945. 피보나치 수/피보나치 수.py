e19ae8aa49c01d02d583819f8bc98bfae02f8b64
HoooDev <97590462+HoooDev@users.noreply.github.com>
2023-02-10 09:59:49 +0900

    [level 2] Title: 피보나치 수, Time: 456.87 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,11 +1,11 @@
 def solution(n):
     answer = 0
-    # 0 1 1 2 3 5 8 13 21 34 ...
     stack = [0, 1]
     
-    for i in range(100001):
+    for i in range(n+1):
         stack.append(stack[-1] + stack[-2])
         if i == n:
             answer = stack[n] % 1234567
+            # break
     
     return answer
